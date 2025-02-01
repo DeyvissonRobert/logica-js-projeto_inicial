5e28d73a5e20a2419ab854ea1450c9571d6f838c
DeyvissonRobert <robert.designerx@gmail.com>
2025-01-31 23:20:46 -0300

    Projeto "finalizado"

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-alert('Boas vindas ao nosso site!');
+let maximo = 107; 
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = parseInt(Math.random() * maximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-let nome = "";
-let idade = "";
-
-let numeroDeVendas = 50;
-let saldoDisponivel = 1000;
-
-let mensagemDeErro = "Erro! Preencha todos os campos"
-
-alert(mensagemDeErro);
-
-let pergunta1 = prompt('Qual o nome do usuário?');
-
-    console.log(nome, "Antes");
-    console.log(nome = pergunta1);
-    console.log(nome, "Depois");
-
-    let pergunta2 = prompt('Digite sua idade');
-
-    console.log(idade, "Antes");
-    console.log(idade = pergunta2);
-    console.log(idade, "Depois");
-
-    if (pergunta2 >= 18) {
-        alert ("Pode tirar a habilitação!")
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${maximo}`);
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
     }
+}
 
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
